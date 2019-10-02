3376b7430344530f2613abd3da120f5e8de81925
Wenley Tong <wenley.tong@gmail.com>
2019-10-01 22:44:11 -0700

    Add single-loader for dependencies

---
+++ ruby/lib/bitemporal.rb
@@ -0,0 +1,6 @@
+require 'bitemporal/immutable_record'
+require 'bitemporal/versioned'
+require 'bitemporal/timeline_event'
+require 'bitemporal/timeline'
+require 'bitemporal/model'
+require 'bitemporal/associations'
