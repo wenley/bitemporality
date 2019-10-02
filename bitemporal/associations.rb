f38d758a984d321b24300a564cd518aa6ec6b633
Wenley Tong <wenley.tong@gmail.com>
2019-10-01 21:24:26 -0700

    Fill in Association methods
    Still untested

--- bitemporal/associations.rb
+++ bitemporal/associations.rb
@@ -10,18 +10,42 @@ module Bitemporal
       if !(active_record_class <= ActiveRecord::Base)
         raise ArgumentError, 'Bitemporal::Associations classes should inherit from ActiveRecord::Base'
       end
+
+      active_record_class.instance_eval(ClassMethods)
     end
 
-    def self.belongs_to_bitemporal(association_name)
+    module ClassMethods
+      def belongs_to_bitemporal(association_name, model:)
+        define_method(association_name) do |transaction_time:, effective_time:|
+          uuid = send("#{association_name}_uuid")
 
-    end
+          model.at_time(uuid: uuid, transaction_time: transaction_time, effective_time: effective_time)
+        end
+      end
 
-    def self.has_one_bitemporal(association_name)
+      def has_one_bitemporal(association_name, model:, foreign_key: nil)
+        define_method(association_name) do |transaction_time:, effective_time:|
+          foreign_key ||= "#{self.name.snakecase}_uuid"
 
-    end
+          model.query(
+            transaction_time: transaction_time,
+            effective_time: effective_time,
+            inner_query: ->(relation) { relation.where(foreign_key => self.uuid) },
+          ).first
+        end
+      end
 
-    def self.has_many_bitemporal(association_name)
+      def has_many_bitemporal(association_name)
+        define_method(association_name) do |transaction_time:, effective_time:|
+          foreign_key ||= "#{self.name.snakecase}_uuid"
 
+          model.query(
+            transaction_time: transaction_time,
+            effective_time: effective_time,
+            inner_query: ->(relation) { relation.where(foreign_key => self.uuid) },
+          )
+        end
+      end
     end
   end
 end
