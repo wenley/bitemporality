15ed19f3ecbf8b834da5543a5e861d71862ed03c
Brian Buchalter <brian.buchalter@gusto.com>
2019-10-02 18:51:59 -0600

    Add test against active record model

--- ruby/spec/immutable_record_spec.rb
+++ ruby/spec/immutable_record_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 RSpec.describe Bitemporal::ImmutableRecord do
-  it { is_expected.to be }
+  before do
+    ActiveRecord::Base.establish_connection(
+      adapter:  'sqlite3',
+      database: 'tmp/db/test.sqlite3',
+    )
+    ActiveRecord::Base.connection.execute(
+      <<-SQL
+        CREATE TABLE IF NOT EXISTS immutable_addresses (
+          street_1 TEXT
+        )
+      SQL
+    )
+  end
+  class ImmutableAddress < ActiveRecord::Base
+    include Bitemporal::ImmutableRecord
+  end
+
+  let(:instance) do
+    ImmutableAddress.create!(
+      street_1: '1 Infinite Loop',
+    )
+  end
+
+  context 'updating' do
+    it 'fails' do
+      expect(instance.update(street_1: 'Anything')).to be_falsey
+    end
+  end
 end
