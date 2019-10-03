713a59a49b6a113e69a481403ab3949a4cc46af9
Brian Buchalter <brian.buchalter@gusto.com>
2019-10-02 18:20:19 -0600

    Demonstrate working rspec

---
+++ ruby/spec/immutable_record_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+RSpec.describe Bitemporal::ImmutableRecord do
+  it { is_expected.to be }
+end
---
+++ ruby/spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'bitemporal'
+
+RSpec.configure do |config|
+
+end
